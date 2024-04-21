facc50f4a1b8d216e96503127c1614bcd54f0c93
claymane <austinclayprofessional@gmail.com>
2024-04-21 17:31:15 -0400

    not so

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,4 +1,4 @@
-# THE ultimate DEFACTO Shipping Cost Calculator
+# THE not so ultimate DEFACTO Shipping Cost Calculator
 
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))
